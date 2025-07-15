dfcfff888263f03fb76a61df9c06c92f7a77f74b
jon apiscope <jon.apiscope@gmail.com>
2025-07-14 22:43:53 -0400

    se agrega backend

--- src/app/core/config/app-config.development.ts
+++ src/app/core/config/app-config.development.ts
@@ -1,6 +1,7 @@
 import { AppConfig } from './app-config.interface';
 
 export const APP_DEV_CONFIG: AppConfig = {
-  apiUrl: 'http://localhost:8000',
+  /* apiUrl: 'http://localhost:8000', */
+  apiUrl: 'https://medidas-back-production.up.railway.app',
   production: false,
 };
--- src/app/core/config/app-config.production.ts
+++ src/app/core/config/app-config.production.ts
@@ -1,6 +1,6 @@
 import { AppConfig } from './app-config.interface';
 
 export const APP_PROD_CONFIG: AppConfig = {
-  apiUrl: 'https://api.tuaplicacion.com/api',
+  apiUrl: 'https://medidas-back-production.up.railway.app',
   production: true,
 };
