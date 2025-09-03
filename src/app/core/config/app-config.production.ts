70dea5b9c8272bc575a488cbf2d3d5e192e7635c
jon apiscope <jon.apiscope@gmail.com>
2025-09-02 21:55:30 -0400

    environment  a HTTPS

--- src/app/core/config/app-config.production.ts
+++ src/app/core/config/app-config.production.ts
@@ -1,6 +1,7 @@
 import { AppConfig } from './app-config.interface';
 
 export const APP_PROD_CONFIG: AppConfig = {
-  apiUrl: 'http://5.189.156.5:4600',
+  apiUrl: 'https://back-medidas.agencia-apiscope.online',
+  //apiUrl: 'http://5.189.156.5:4600',
   production: true,
 };
