ce0939434bc07fb9ebba6b5622046a639b41f038
jon apiscope <jon.apiscope@gmail.com>
2025-10-22 18:48:32 -0400

    se agrega nueva url de back

--- src/app/core/config/app-config.development.ts
+++ src/app/core/config/app-config.development.ts
@@ -1,7 +1,7 @@
 import { AppConfig } from './app-config.interface';
 
 export const APP_DEV_CONFIG: AppConfig = {
-  apiUrl: 'https://back-medidas.agencia-apiscope.online',
+  apiUrl: 'https://back-medidas.nex-gen.digital',
   //apiUrl: 'http://5.189.156.5:4600',
   /* apiUrl: 'https://medidas-back-production.up.railway.app', */
   production: false,
